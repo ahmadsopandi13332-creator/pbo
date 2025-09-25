c1ce4e874905205c24f0575055885e2ba3505676
Ahmad Sopandi <ahmadsopandi13332@gmail.com>
2025-09-25 11:26:07 +0700

    Create 03.js

---
+++ 03.js
@@ -0,0 +1,51 @@
+// Class Induk
+class Mahasiswa {
+  constructor(nama, nim, jurusan) {
+    this.nama = nama;       // attribute 1
+    this.nim = nim;         // attribute 2
+    this.jurusan = jurusan; // attribute 3
+  }
+
+  belajar() { // method 1
+    console.log(`${this.nama} sedang belajar.`);
+  }
+
+  tampilkanData() { // method 2
+    console.log(`Nama: ${this.nama}, NIM: ${this.nim}, Jurusan: ${this.jurusan}`);
+  }
+}
+
+// Class Anak 1
+class MahasiswaAktif extends Mahasiswa {
+  constructor(nama, nim, jurusan, angkatan) {
+    super(nama, nim, jurusan);
+    this.angkatan = angkatan; // attribute 4
+  }
+
+  ikutOrganisasi() { // method 3
+    console.log(`${this.nama} ikut organisasi kampus.`);
+  }
+}
+
+// Class Anak 2
+class MahasiswaAlumni extends Mahasiswa {
+  constructor(nama, nim, jurusan, tahunLulus) {
+    super(nama, nim, jurusan);
+    this.tahunLulus = tahunLulus; // attribute 5
+  }
+
+  kerja() { // method 4
+    console.log(`${this.nama} sudah bekerja setelah lulus.`);
+  }
+}
+
+// Buat 3 Object
+const mhs1 = new Mahasiswa("Ahmad Sopandi", "12345", "Informatika");
+const mhs2 = new MahasiswaAktif("Budi", "67890", "Sistem Informasi", 2023);
+const mhs3 = new MahasiswaAlumni("Citra", "11223", "Teknik Industri", 2022);
+
+// Panggil Method
+mhs1.belajar();         // method 1
+mhs1.tampilkanData();   // method 2
+mhs2.ikutOrganisasi();  // method 3
+mhs3.kerja();           // method 4
